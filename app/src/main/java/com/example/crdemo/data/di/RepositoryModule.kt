c931c0c7e011fadad4285ce4c006e14831bc78e1
Yukaichen <YuKaichen@micb2b.com>
2025-03-12 07:31:16 +0800

    refactor(): add

--- app/src/main/java/com/example/crdemo/data/di/RepositoryModule.kt
+++ app/src/main/java/com/example/crdemo/data/di/RepositoryModule.kt
@@ -4,6 +4,7 @@ package com.example.crdemo.data.di
 import com.example.crdemo.data.dao.AnimalDao
 import com.example.crdemo.data.dao.ExhibitDao
 import com.example.crdemo.data.dao.PlantDao
+import com.example.crdemo.data.repository.ZooRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,8 +21,8 @@ object RepositoryModule {
         animalDao: AnimalDao,
         plantDao: PlantDao,
         exhibitDao: ExhibitDao
-    ): DataRepository {
-        return DataRepository(animalDao, plantDao, exhibitDao)
+    ): ZooRepository {
+        return ZooRepository(animalDao, plantDao, exhibitDao)
     }
 
 }
--- app/src/main/java/com/example/crdemo/data/repository/ZooRepository.kt
+++ app/src/main/java/com/example/crdemo/data/repository/ZooRepository.kt
@@ -1,6 +1,9 @@
 package com.example.crdemo.data.repository
 
 
+import com.example.crdemo.data.dao.AnimalDao
+import com.example.crdemo.data.dao.ExhibitDao
+import com.example.crdemo.data.dao.PlantDao
 import com.example.crdemo.data.model.AnimalResponse
 import com.example.crdemo.data.model.PlantsResponse
 import com.example.crdemo.data.model.ZooResponse
@@ -9,7 +12,11 @@ import com.google.gson.Gson
 import retrofit2.Response
 
 
-class ZooRepository {
+class ZooRepository(
+    private val animalDao: AnimalDao,
+    private val plantDao: PlantDao,
+    private val exhibitDao: ExhibitDao
+) {
     fun parseJson(jsonString: String): ZooResponse {
         val gson = Gson()
         return gson.fromJson(jsonString, ZooResponse::class.java)
